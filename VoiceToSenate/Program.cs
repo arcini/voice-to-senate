48fd765b60705dd9f9c1f591f2f35bed3ba09bb8
Alex Cini <arcini8@gmail.com>
2025-05-20 18:52:36 -0400

    Syncfusion Blazor setup (#2)
    Yeah

--- VoiceToSenate/Program.cs
+++ VoiceToSenate/Program.cs
@@ -1,10 +1,25 @@
 using VoiceToSenate.Components;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSyncfusionBlazor();
+
+var syncfusionLicenseKey = builder.Configuration["Syncfusion:LicenseKey"];
+
+if (!string.IsNullOrWhiteSpace(syncfusionLicenseKey))
+{
+    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncfusionLicenseKey);
+}
+else
+{
+    throw new Exception("Syncfusion License Key is missing.");
+}
+
 
 var app = builder.Build();
 
